8a120b19167b5bc4ac1e7d44b1ff7078ba38be10
REJack <info@rejack.de>
2017-12-19 11:13:00 +0100

    updated grunt tasks & added grunt modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,35 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-      options: {
-        globals: {
-          jQuery: true
+  var colors = ['black', 'black-light', 'blue', 'blue-light', 'green', 'green-light', 'purple', 'purple-light', 'red', 'red-light', 'yellow', 'yellow-light'];
+  var copy_output = [];
+
+  colors.forEach(function (element, index){
+    copy_output.push({
+        [element]: {
+          expand: true,
+          path: ['BASE/**'],
+          dest: 'f_'+element+'/',
         }
-      }
-    },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint']
-    }
+    });
   });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.initConfig({
+    move: {
+      assets: {
+        src: 'assets',
+        dest: 'BASE/assets/'
+      },
+      other: {
+        src: ['*.tpl', 'README.md', 'package.json', 'LICENSE'],
+        dest: 'BASE/'
+      }
+    },
+    copy: copy_output
+  });
 
-  grunt.registerTask('default', ['jshint']);
 
+  grunt.loadNpmTasks('grunt-move');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerTask('default', ['move', 'copy']);
 };
